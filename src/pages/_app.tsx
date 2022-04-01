2995b3a7b3ee99b0081c38d29205aa466ac70317
Vitor Gouveia <vitorneves.gouveia10@gmail.com>
2022-04-01 18:01:32 -0300

    chore: add basic next.js app

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import type { AppProps } from "next/app"
+
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <Component {...pageProps} />
+  )
+}
+
+export default App
